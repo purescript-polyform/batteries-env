64ec4988297acf1006797c369465e04827302781
Tomasz Rybarczyk <paluho@gmail.com>
2020-10-15 02:17:57 +0200

    Drop array / arrayOf

--- spago.dhall
+++ spago.dhall
@@ -3,7 +3,8 @@ Welcome to a Spago project!
 You can edit this file as you like.
 -}
 { name = "my-project"
-, dependencies = [ "console", "effect", "polyform-batteries", "psci-support" ]
+, dependencies =
+  [ "console", "debug", "effect", "polyform-batteries", "psci-support", "spec" ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
 }
--- src/Polyform/Batteries/Env/Validators.purs
+++ src/Polyform/Batteries/Env/Validators.purs
@@ -1,9 +1,7 @@
 -- | You can use `Batteries.Integer.validator` or `Batteries.Number.validator`
 -- | directly and other `String` based validators as field value validator like:
 module Polyform.Batteries.Env.Validators
-  ( array
-  , arrayOf
-  , boolean
+  ( boolean
   , Field
   , FieldValue
   , MissingValue
@@ -17,21 +15,12 @@ module Polyform.Batteries.Env.Validators
 
 import Prelude
 
-import Data.Array (singleton) as Array
-import Data.Bifunctor (lmap)
 import Data.Identity (Identity)
 import Data.Map (lookup) as Map
 import Data.Maybe (Maybe(..))
-import Data.String.Regex (Regex)
-import Data.String.Regex (split) as String.Regex
-import Data.String.Regex.Unsafe (unsafeRegex)
-import Data.Traversable (sequence)
-import Data.TraversableWithIndex (traverseWithIndex)
-import Data.Validation.Semigroup (V)
-import Data.Variant (inj)
-import Polyform.Batteries (Errors, Validator, invalid) as Batteries
+import Polyform.Batteries (Validator, invalid) as Batteries
 import Polyform.Batteries.Env.Types (Env, Key, Value, Validator, fromValidator)
-import Polyform.Validator (liftFn, liftFnMV, liftFnV, lmapValidator, runValidator)
+import Polyform.Validator (liftFn, liftFnMV, liftFnV, runValidator)
 import Type.Prelude (SProxy(..))
 import Type.Row (type (+))
 
@@ -85,41 +74,4 @@ boolean = liftFnV case _ of
   "true" → pure true
   "false" → pure false
   v → Batteries.invalid _booleanExpected v
-
--- | Source: https://stackoverflow.com/a/35040702/194614
--- | TODO: make separator configurable.
-separatorRegex ∷ Regex
-separatorRegex = unsafeRegex """(?<!(?<![^\\]\\(?,\\{2}){0,10})\\)""" mempty
-
-array ∷ ∀ e m. Monad m ⇒ FieldValue m e (Array String)
-array = liftFn case _ of
-  "" → []
-  v → String.Regex.split separatorRegex v
-
-
-_invalidItems = SProxy ∷ SProxy "invalidItems"
-
-type ItemErrors fe = { index ∷ Int, error ∷ Batteries.Errors fe }
-type InvalidItems fe e = (invalidItems ∷ Array (ItemErrors fe) | e)
-
-arrayOf
-  ∷ ∀ m e fe a
-  . Monad m
-  ⇒ FieldValue m fe a
-  → FieldValue m (InvalidItems fe + e) (Array a)
-arrayOf v = array >>> liftFnMV av
-  where
-    ep idx error = { index:  idx, error }
-
-    -- | Run every validator by prefixing its error path with index.
-    -- | Validator results in `m (V e a)` so we need traverse here.
-    f ∷ Array String → m (Array (V (ItemErrors fe) a))
-    f = traverseWithIndex \idx → runValidator (lmapValidator (ep idx) v)
-
-
-    av ∷ Array String → m (V (Batteries.Errors (InvalidItems fe + e)) (Array a))
-    av = f >>> map liftErrs
-      where
-      liftErrs ∷ Array (V (ItemErrors fe) a) → V (Batteries.Errors (InvalidItems fe + e)) (Array a)
-      liftErrs = map (lmap Array.singleton) >>> sequence >>> lmap (inj _invalidItems >>> Array.singleton)
 
