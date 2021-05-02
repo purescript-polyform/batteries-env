e9083c6ccecd35ec8ae32abc1f555cbf1131ea89
Tomasz Rybarczyk <paluho@gmail.com>
2021-05-02 08:39:41 +0200

    s/Error/Msg/

--- src/Polyform/Batteries/Env/Types.purs
+++ src/Polyform/Batteries/Env/Types.purs
@@ -4,7 +4,7 @@ import Prelude
 
 import Data.Array (singleton) as Array
 import Data.Map (Map)
-import Polyform.Batteries (Dual, Validator, Error) as Batteries
+import Polyform.Batteries (Dual, Validator, Errors) as Batteries
 import Polyform.Dual (hoistParser) as Dual
 import Polyform.Validator (Validator) as Polyform
 import Polyform.Validator (lmapValidator)
@@ -17,7 +17,7 @@ type Value = String
 
 type Env = Map Key Value
 
-type Errors (errs ∷ # Type) = Array { key ∷ Key, errors ∷ Array (Batteries.Error errs) }
+type Errors (errs ∷ # Type) = Array { key ∷ Key, errors ∷ Batteries.Errors errs }
 
 type Validator m (errs ∷ # Type) i o = Polyform.Validator m (Errors errs) i o
 
